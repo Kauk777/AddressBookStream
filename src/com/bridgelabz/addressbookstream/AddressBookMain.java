edb3402f3c8280c64b825d8bceab5792fef867fd
Kauk777 <curokky777@gmail.com>
2020-10-05 20:46:32 +0530

    [KAUSHAL]Added: AddressBook using stream

---
+++ src/com/bridgelabz/addressbookstream/AddressBookMain.java
@@ -0,0 +1,90 @@
+package com.bridgelabz.addressbookstream;
+
+import java.util.*;
+
+
+public class AddressBookMain {
+	
+    private Map<String,Person> contactMap;
+	
+	public AddressBookMain() {
+		contactMap = new HashMap<>();
+	}
+	
+	public void addDetails(String fname,String lname, String address, String city, String state, String zip, long phno, String email) {
+		Person p=new Person(fname,lname,address,city,state,zip,phno,email);
+		String name=fname+" "+lname;
+		contactMap.put(name, p);
+	}
+	
+	public void viewDetails() {
+		System.out.println(contactMap.size()+" entries");
+		for(Map.Entry<String,Person> m:contactMap.entrySet()) {
+			System.out.println(m.getKey());
+			System.out.println(m.getValue());
+		}
+	}
+	
+	public void editDetails(String fname, String lname) {
+		for(Map.Entry<String,Person> m:contactMap.entrySet()) {
+			if(m.getValue().fName.equals(fname) && m.getValue().lName.equals(lname)) {
+				Person editedDetails=new Person(fname,lname,"Dew Point","Melbourne","Queens","P2548",785412,"joshua@hotmail.com");
+				contactMap.replace(m.getKey(),editedDetails);
+				System.out.println("Edited Details of "+fname+" "+lname+": "+editedDetails);
+			}
+				
+		}
+	}
+	
+	public void deleteDetails(String fname, String lname) {
+		for(Map.Entry<String,Person> m:contactMap.entrySet()) {
+			if(m.getValue().fName.equals(fname) && m.getValue().lName.equals(lname)) {
+				System.out.println("Deleted "+fname+" "+lname+" address");
+				contactMap.remove(m.getKey());
+				return;
+			}
+				
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		AddressBookMain addBook=new AddressBookMain();
+		addBook.addDetails("Joshua", "Patrick", "Mave Hill 025", "Las Vegas", "California", "C25042", 25478963, "joshua@green.com");
+		addBook.addDetails("Staya", "Veda", "Kali Marg 254", "Kolkata", "West Bengal", "254156", 75848933, "veda@gmail.com");
+		addBook.addDetails("Zao", "Vikigh", "Yellow River R10", "Xinju", "Bejing", "745156", 98848933, "zao@gmail.com");
+		addBook.addDetails("Zao", "Vikigh", "Yellow Plain R10", "Xinju", "Bejing", "745156", 98848933, "zao@gmail.com");
+		addBook.addDetails("Momba", "High", "Venus street", "Panji", "Goa", "831005", 84512355, "high@hotmail.com");
+		addBook.viewDetails();
+		boolean flag=true;
+		System.out.println("1. Edit address detail based on person name");
+		System.out.println("2. Delete address detail based on person name");
+		System.out.println("3. Exit");
+		while(flag) {
+			System.out.println("Enter your choice");
+			int ch=sc.nextInt();
+			switch(ch) {
+			case 1: 
+				System.out.println("Enter name to edit its address");
+				String fname=sc.next();
+				String lname=sc.next();
+				addBook.editDetails(fname,lname);
+				break;
+			case 2:
+				System.out.println("Enter name to delete its address");
+				String fdname=sc.next();
+				String ldname=sc.next();
+				addBook.deleteDetails(fdname,ldname);
+				break;
+			case 3:
+				flag=false;
+				break;
+			default:
+				System.out.println("Enter valid option");
+			}
+		}
+		System.out.println("New address details");
+		addBook.viewDetails();
+	}
+
+}
---
+++ src/com/bridgelabz/addressbookstream/Person.java
@@ -0,0 +1,46 @@
+package com.bridgelabz.addressbookstream;
+
+public class Person {
+	public String fName;
+	public String lName;
+	private String address;
+	private String city;
+	private String state;
+	private String zip;
+	private long phoneNo;
+	private String email;
+	
+	public Person(String fName, String lName, String address, String city, String state, String zip, long phoneNo,
+			String email) {
+		this.fName = fName;
+		this.lName = lName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.phoneNo = phoneNo;
+		this.email = email;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	@Override
+	public String toString() {
+		return "Person Address [FirstName=" + fName + ", LastName=" + lName + ", address=" + address + ", city=" + city + ", state="
+				+ state + ", zip=" + zip + ", phoneNo=" + phoneNo + ", email=" + email + "]";
+	}
+}
